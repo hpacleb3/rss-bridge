d88d066a5cee137ecd2995a1f2098553cab91187
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2019-07-24 17:39:37 +0800

    Update NexonBridge.php

--- bridges/NexonBridge.php
+++ bridges/NexonBridge.php
@@ -9,7 +9,7 @@ class NexonBridge extends BridgeAbstract {
  	public function collectData() {
 			$html = getSimpleHTMLDOM(self::URI)
 			or returnServerError('Could not request Elsword Nexon Patch Notes.');
-		foreach($html->find('h3.subject') as $element){
+		foreach($html->find('h3.subject a') as $element){
 			$item = array();
 			$item['title'] = $element.innerText;
 			$item['uri'] = $element.href;
