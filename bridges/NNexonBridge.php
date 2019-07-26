cf85b6185e2d87a582f26fb877031dcbb2c52ccb
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2019-07-26 14:18:16 +0800

    Create NNexonBridge.php

---
+++ bridges/NNexonBridge.php
@@ -0,0 +1,26 @@
+<?php
+class NNexonBridge extends BridgeAbstract {
+	const NAME        = 'Elsword Patch 2 Nexon Bridge';
+	const URI         = 'http://elsword.nexon.com/news/notice/list.aspx?emNoticeCode=Inspection';
+	const DESCRIPTION = 'Returns Latest KR Elsword Patch news';
+	const MAINTAINER  = 'me trying to do this';
+	const CACHE_TIMEOUT = 60;
+  
+ 	public function collectData() {
+			$html = getSimpleHTMLDOM(self::URI)
+			or returnServerError('Could not request Elsword Nexon Patch Notes.');
+		foreach($html->find('h3.subject a') as $element){
+			$item = array();
+			$uri = $element->href;
+			$title = $element->plaintext;
+			$item['title'] = $title;
+			$item['uri'] = $uri;
+			
+			$this->items[] = $item;
+			
+		}
+		
+
+	}
+  
+}
