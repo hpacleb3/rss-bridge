00d1504c07dc3d51fe1fa5e22876f28442fd3d4a
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2019-08-31 20:05:42 +0800

    Update ElSliderBridge.php

--- bridges/ElSliderBridge.php
+++ bridges/ElSliderBridge.php
@@ -1,1 +1,22 @@
-
+<?php
+class ElSliderBridge extends BridgeAbstract {
+	const NAME        = 'Elsword Slider Bridge';
+	const URI         = 'http://launcher.elswordonline.com/en/';
+	const DESCRIPTION = 'Returns Latest NA Elsword Sliders';
+	const MAINTAINER  = 'me trying to do this';
+	const CACHE_TIMEOUT = 60;
+  
+ 	public function collectData() {
+			$html = getSimpleHTMLDOMCached(self::URI,60)
+			or returnServerError('Could not request Elsword Slider Data.');
+		foreach($html->find('li.tabCot a') as $element){
+			$item = array();
+			$uri = $element->href;
+			$item['uri'] = $uri;
+			
+			$this->items[] = $item;
+			
+		}
+		
+	}
+  
