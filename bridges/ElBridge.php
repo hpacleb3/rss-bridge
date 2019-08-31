9f1703aa926d046956eba2d318d7f612f79a98a9
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2019-08-31 20:14:21 +0800

    Update and rename ElSliderBridge.php to ElBridge.php

--- bridges/ElBridge.php
+++ bridges/ElBridge.php
@@ -1,17 +1,19 @@
 <?php
-class ElSliderBridge extends BridgeAbstract {
-	const NAME        = 'Elsword Slider Bridge';
+class ElBridge extends BridgeAbstract {
+	const NAME        = 'Elsword Patch Nexon Bridge';
 	const URI         = 'http://launcher.elswordonline.com/en/';
-	const DESCRIPTION = 'Returns Latest NA Elsword Sliders';
+	const DESCRIPTION = 'Returns Latest Slider Elsword news';
 	const MAINTAINER  = 'me trying to do this';
 	const CACHE_TIMEOUT = 60;
   
  	public function collectData() {
 			$html = getSimpleHTMLDOMCached(self::URI,60)
-			or returnServerError('Could not request Elsword Slider Data.');
+			or returnServerError('Could not request Elsword Nexon Patch Notes.');
 		foreach($html->find('li.tabCot a') as $element){
 			$item = array();
 			$uri = $element->href;
+			$title = $element->plaintext;
+			$item['title'] = $title;
 			$item['uri'] = $uri;
 			
 			$this->items[] = $item;
@@ -20,3 +22,4 @@ class ElSliderBridge extends BridgeAbstract {
 		
 	}
   
+}
