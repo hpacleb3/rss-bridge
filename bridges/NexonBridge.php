1567b987ee30281f1be0fa74b81d312b26a9ce18
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2020-02-26 10:56:42 +0800

    Update NexonBridge.php

--- bridges/NexonBridge.php
+++ bridges/NexonBridge.php
@@ -16,7 +16,8 @@ class NexonBridge extends BridgeAbstract {
 			$item['content'] = $title;
 			$item['title'] = $uri;
 			$item['uri'] = $uri;
-			$articleHTML = getSimpleHTMLDOMCached($item['uri']);
+			//doesnt work
+		// 	$articleHTML = getSimpleHTMLDOMCached($item['uri']);
 		//	$getDate = $articleHTML->find('div.bv_date span.date',0)->plaintext;
 		//	date_default_timezone_set('Asia/Seoul');
 		//	$test  = str_replace(" 오후 "," ", $getDate);
