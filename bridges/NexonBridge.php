b4e717ebc94927a4cbbd924147fcf697a67f7080
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2020-02-26 12:37:42 +0800

    Update NexonBridge.php

--- bridges/NexonBridge.php
+++ bridges/NexonBridge.php
@@ -15,8 +15,8 @@ class NexonBridge extends BridgeAbstract {
 			$articleHTML = getSimpleHTMLDOMCached($uri)
 				or returnServerError('Could not request ' . $uri);
 			//error around here
-			$getDate = $articleHTML->find('span.date');
-			$test  = str_replace(" 오후 "," Asia/Seoul ", $getDate[0]->plaintext);
+			$getDate = $articleHTML->find('span.date',0);
+			$test  = str_replace(" 오후 "," Asia/Seoul ", $getDate->plaintext);
 			$this['timestamp'] = strtotime($test);
 			$title = $element->plaintext;
 			$item['content'] = $title;
