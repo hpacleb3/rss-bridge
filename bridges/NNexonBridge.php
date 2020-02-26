13e3bc108350302093b68da112896eb675f83a91
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2020-02-26 15:14:09 +0800

    Update NNexonBridge.php

--- bridges/NNexonBridge.php
+++ bridges/NNexonBridge.php
@@ -4,24 +4,29 @@ class NNexonBridge extends BridgeAbstract {
 	const URI         = 'http://elsword.nexon.com/news/notice/list.aspx?emNoticeCode=Inspection';
 	const DESCRIPTION = 'Returns Latest KR Elsword Patch news';
 	const MAINTAINER  = 'me trying to do this';
-	const CACHE_TIMEOUT = 60;
+	const CACHE_TIMEOUT = 300;
   
  	public function collectData() {
-			$html = getSimpleHTMLDOM(self::URI)
-			or returnServerError('Could not request Elsword Nexon Patch Notes.');
+			$html = getSimpleHTMLDOMCached(self::URI,300)
+				or returnServerError('Could not request Elsword Nexon Patch Notes.');
 		foreach($html->find('h3.subject a') as $element){
-			$item = array();
 			$uri = $element->href;
+			$item['uri'] = $uri;
 			$title = $element->plaintext;
 			$item['content'] = $title;
 			$item['title'] = $uri;
-			$item['uri'] = $uri;
-			
+			DEBUG::log($uri);
+			$articleHTML = getSimpleHTMLDOMCached($uri)
+				or returnServerError('Could not request ' . $uri);
+			$getDate = $articleHTML->find('span.date',0)->plaintext;
+			DEBUG::log($getDate);
+			$getDate = (string)$getDate;
+			$getDate = str_replace(' 오후 ',' Asia/Seoul ', $getDate);
+			$lastDate = strtotime($getDate);
+			$item['timestamp'] = $lastDate; // Error: Cannot use object of type NexonBridge as array in /app/bridges/NexonBridge.php
 			$this->items[] = $item;
 			
 		}
-		
-
 	}
   
 }
