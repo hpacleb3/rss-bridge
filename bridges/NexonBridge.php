9802d85cb4ac5597eb6a367befee955d6fbc9b7e
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2020-03-05 17:39:24 +0800

    Update NexonBridge.php

--- bridges/NexonBridge.php
+++ bridges/NexonBridge.php
@@ -4,10 +4,10 @@ class NexonBridge extends BridgeAbstract {
 	const URI         = 'http://elsword.nexon.com/news/update/list.aspx';
 	const DESCRIPTION = 'Returns Latest KR Elsword Patch news';
 	const MAINTAINER  = 'me trying to do this';
-	const CACHE_TIMEOUT = 300;
+	const CACHE_TIMEOUT = 60;
   
  	public function collectData() {
-			$html = getSimpleHTMLDOMCached(self::URI,300)
+			$html = getSimpleHTMLDOMCached(self::URI,60)
 				or returnServerError('Could not request Elsword Nexon Patch Notes.');
 		$i = 1;
 		foreach($html->find('h3.subject a') as $element){
