6520822641b894bfdd38d7936ae1571c62f28530
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2020-03-05 17:39:08 +0800

    Create NNNexonBridge.php

---
+++ bridges/NNNexonBridge.php
@@ -0,0 +1,34 @@
+<?php
+class NNNexonBridge extends BridgeAbstract {
+	const NAME        = 'Elsword Patch Nexon Bridge';
+	const URI         = 'http://elsword.nexon.com/news/notice/list.aspx?emNoticeCode=Notice';
+	const DESCRIPTION = 'Returns Latest KR Elsword Patch news';
+	const MAINTAINER  = 'me trying to do this';
+	const CACHE_TIMEOUT = 60;
+  
+ 	public function collectData() {
+			$html = getSimpleHTMLDOMCached(self::URI,60)
+				or returnServerError('Could not request Elsword Nexon Patch Notes.');
+		$i = 1;
+		foreach($html->find('h3.subject a') as $element){
+			
+			$uri = $element->href;
+			$item['uri'] = $uri;
+			$title = $element->plaintext;
+			$item['content'] = $title;
+			$item['title'] = $uri;
+			DEBUG::log($uri);
+			$articleHTML = getSimpleHTMLDOMCached($uri)
+				or returnServerError('Could not request ' . $uri);
+			$getDate = $articleHTML->find('span.date',0)->plaintext;
+			DEBUG::log($getDate);
+			$getDate = (string)$getDate;
+			$getDate = str_replace(' 오후 ',' Asia/Seoul ', $getDate);
+			$lastDate = strtotime($getDate);
+			$item['timestamp'] = $lastDate; // Error: Cannot use object of type NexonBridge as array in /app/bridges/NexonBridge.php
+			$this->items[] = $item;
+			if ($i++ == 5) break;
+		}
+	}
+  
+}
