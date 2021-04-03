61f630a8c009e091ccbd8b5498acf3dc05a0ba26
hpacleb3 <53234009+hpacleb3@users.noreply.github.com>
2021-04-03 12:40:31 +0800

    Update NNNexonBridge.php

--- bridges/NNNexonBridge.php
+++ bridges/NNNexonBridge.php
@@ -12,8 +12,8 @@ class NNNexonBridge extends BridgeAbstract {
 		$i = 1;
 		foreach($html->find('ul.board_list li') as $element){
 			
-			$uri= $element->find(' a, 0')->href;
-			$subject = $element->find('span.subject, 0')->plaintext;
+			$uri= $element->find(' a', 0)->href;
+			$subject = $element->find('span.subject', 0)->plaintext;
 			$item['uri'] = $uri;
 			$item['content'] = $subject;
 			$item['title'] = $uri;
@@ -21,7 +21,7 @@ class NNNexonBridge extends BridgeAbstract {
 			$articleHTML = getSimpleHTMLDOMCached($uri)
 				or returnServerError('Could not request ' . $uri);
 			$getDate = $articleHTML->find('i_date', 0);
-			$readDate = $getDate->find('span.data, 0')->plaintext;
+			$readDate = $getDate->find('span.data', 0)->plaintext;
 			DEBUG::log($getDate);
 			$readDate = (string)$readDate;
 			$readDate = str_replace('.','-',$readDate);
